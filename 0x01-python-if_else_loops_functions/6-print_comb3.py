7d278921749347b9ac81b6151e06bd7e1468060d
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-08 19:29:23 +0100

    combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for number1 in range(0, 10):
     for number2 in range(number1 + 1, 10):
         if (number1 == 8):
-             print('{}{}'.format(number1, number2), end='')
+            print('{}{}'.format(number1, number2), end='')
         else:
-             print('{}{}, '.format(number1, number2), end='')
+            print('{}{}, '.format(number1, number2), end='')
 print()
