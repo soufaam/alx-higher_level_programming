454173cae64846c82eeb9170bb1319f6a0edc628
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-08 19:27:40 +0100

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number1 in range(0, 10):
+    for number2 in range(number1 + 1 , 10):
+        if (number1 == 8):
+             print('{}{}'.format(number1, number2), end='')
+        else:
+             print('{}{}, '.format(number1, number2), end='')
+print()
