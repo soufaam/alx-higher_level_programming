4da228f614b0ebab2639a984e846e83a06ded07f
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-09 10:44:21 +0100

    trinary condtion

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,5 @@
 
 
 for number in range(122, 96, -1):
-    if number % 2 == 0:
-        print(chr(number), end='')
-    else:
-        print(chr(number-32), end='')
+    print('{}'.format(chr(number)
+                      if number % 2 == 0 else chr(number - 32)), end='')
