761142bb76c719ec4f47680ba44415ec4a8d6ce6
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-11 23:28:16 +0100

    0-add 1-cal

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    adding = __import__('add_0')
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, adding.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
