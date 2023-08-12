55d7a5d337b4b4dc62b60a0caf333f838f02610e
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-12 16:30:13 +0100

    fast uppercase

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-from sys import stdout
-stdout.write("#pythoniscool\n")
+import builtins
+out
+
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+__import
+def magic_calculation(a, b):
+    if (a < b):
+        c = add(a, b)
+    
+    
+    
+    
+import dis
+dis.dis('magic_calculation')
