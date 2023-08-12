3f3c4bcd80f38127403d0ff83b679689a18e9687
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-12 10:41:29 +0100

    handling one arg 2-argv.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for iter in range(1, len(sys.argv)):
