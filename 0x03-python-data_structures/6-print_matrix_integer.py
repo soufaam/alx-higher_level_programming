150c9a94941a47f61595e5f08c8c5de96c86557c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-14 23:43:45 +0100

    matrix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for item in matrix:
         for idx in range(len(item)):
             if idx == len(item) - 1:
-                print("{:d}". format(item[idx]), end="")
+                print("{:d}".format(item[idx]), end="")
             else:
-                print("{:d}". format(item[idx]), end=" ")
+                print("{:d}".format(item[idx]), end=" ")
         print()
