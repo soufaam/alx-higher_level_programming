299792680256b7db88a14463435338544d8c6b0e
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-14 23:41:24 +0100

    matrix print

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for item in matrix:
+        for idx in range(len(item)):
+            if idx == len(item) - 1:
+                print("{:d}". format(item[idx]), end="")
+            else:
+                print("{:d}". format(item[idx]), end=" ")
+        print()
