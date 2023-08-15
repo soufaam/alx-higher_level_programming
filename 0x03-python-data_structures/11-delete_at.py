ac6f465b2755b92ca0cee94eeb56b6bd684abdb9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-15 21:15:06 +0100

    switch

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,6 +5,7 @@ def delete_at(my_list=[], idx=0):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
+            my_list.remove(my_list[idx])
             continue
         else:
             nwlist.append(my_list[i])
