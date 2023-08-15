a561ec4cebf482e67359ced5c53edc4b16b0b7d0
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-15 16:33:44 +0100

    delete at index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    nwlist = []
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    for i in range(len(my_list)):
+        if i == idx:
+            continue
+        else:
+            nwlist.append(my_list[i])
+    return nwlist
