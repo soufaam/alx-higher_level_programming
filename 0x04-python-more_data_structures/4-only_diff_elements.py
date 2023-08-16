0a7c2266f78011dc0b954d1c0b22296e687d159b
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-16 21:07:17 +0100

    Only differents element in sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -7,7 +7,8 @@ def only_diff_elements(set_1, set_2):
         return set_1
     for element1 in set_1:
         for element2 in set_2:
-            if element1 != element2:
+            if element1 not in set_2:
                 new_set.add(element1)
+            if element2 not in set_1:
                 new_set.add(element2)
     return new_set
