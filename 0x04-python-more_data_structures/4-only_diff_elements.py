3d7e3ea891592ffc8565ec91a82f6c6a10a8fb40
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-16 19:52:33 +0100

    Only differents element in sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -7,7 +7,3 @@ def only_diff_elements(set_1, set_2):
                 new_set.add(element1)
                 new_set.add(element2)
     return new_set
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
