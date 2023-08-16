2c928e1cd0f1a26c32d714ef8f91a9f9509122c9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-16 20:16:46 +0100

    Only differents element in sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = set()
+    if len(set_1) == 0 and len(set_2):
+        return set_2
+    elif len(set_2) == 0 and len(set_1):
+       return set_1
     for element1 in set_1:
         for element2 in set_2:
             if element1 != element2:
