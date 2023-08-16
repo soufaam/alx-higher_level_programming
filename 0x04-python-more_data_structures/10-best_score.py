4038395702fb330f9a99fdd1e57d8738e4ab43f5
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-16 23:19:51 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    if len(a_dictionary) == 0:
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     best_score = 0
     for key in a_dictionary.keys():
