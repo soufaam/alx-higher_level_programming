4972a34b7a9db8e1fdfb3a3785bc0c70a4757a26
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-17 13:09:27 +0100

    Weighted avrage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     sum = 0
     moy = 0
+    if len(my_list) == 0:
+        return 0
     for item in my_list:
         sum += item[0] * item[1]
         moy += item[1]
