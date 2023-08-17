391f8dd3c0357ba53ab0d552a35227b0c3ad901b
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-17 11:42:37 +0100

    roman numeral

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-   if "str" not in type(roman_string) or roman_string is None:
-       return None
-   lenght = len(roman_string)
-   
-       
+    sum = 0
+    roman = {"X": 10, "V": 5, "C": 100, "D": 500, "L": 50, "I": 1, "M": 1000}
+    if "str" not in str(type(roman_string)) or roman_string is None:
+        return None
+    for idx in range(len(roman_string)):
+        if roman_string[idx] in roman.keys():
+            sum += roman[roman_string[idx]]
+        if roman_string[idx] == "V" or roman_string[idx] == "X":
+            if idx > 0 and roman_string[idx - 1] == "I":
+                sum -= 2
+    return sum
