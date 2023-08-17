d9034af7c19ce20d5049965ef0d6bcbf0f43bb82
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-17 10:23:39 +0100

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     best_score = -99999999999
+    best_key = None
     for key in a_dictionary.keys():
         if best_score < a_dictionary[key]:
-            best_score = key
-    return best_score
+            best_score = a_dictionary[key]
+            best_key = key
+    return best_key
