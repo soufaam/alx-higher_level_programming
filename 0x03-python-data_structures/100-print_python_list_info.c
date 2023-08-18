35663b0e1023932a536c09d8c97d2f32755eecaf
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-19 00:49:07 +0100

    Cython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - check the code
+ * @p: pYOBJECT
+ * Return: Always EXIT_SUCCESS.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyObject *item, *iter;
+	size_t i = 0;
+
+	printf("[*] Size of the Python List = %lu\n", PyList_GET_SIZE(p));
+	printf("[*] Allocated = %lu \n", PyList_GET_SIZE(p));
+	iter = PyObject_GetIter(p);
+	item = PyIter_Next(iter);
+	while (item)
+	{
+		if (PyList_Check(item))
+			printf("Element %lu: list\n", i);
+		else if (PyTuple_Check(item))
+			printf("Element %lu: tuple\n", i);
+		else if (PyLong_Check(item))
+			printf("Element %lu: int\n", i);
+		else if (PyFloat_Check(item))
+			printf("Element %lu: float\n", i);
+		else if (PyUnicode_Check(item))
+			printf("Element %lu: str\n", i);
+		item = PyIter_Next(iter);
+		i++;
+	}
+}
