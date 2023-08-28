b95c94c52ec5cd7458d1cf73692c729e7cf658f9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 00:19:52 +0100

    ValueError exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[idx]), end='')
                 number += 1
-            except:
+            except ValueError:
+                pass
+            except TypeError:
                 pass
         print()
     except IndexError:
