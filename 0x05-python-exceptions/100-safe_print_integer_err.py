fb0070076c1a54958ff4f9cfcc7d388e41f25973
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 01:50:38 +0100

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -14,17 +14,3 @@ def safe_print_integer_err(value):
  'd' for object of type 'str'\n")
         return False
     return True
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
