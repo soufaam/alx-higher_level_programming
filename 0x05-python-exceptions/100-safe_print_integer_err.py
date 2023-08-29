40e2b783c409661ea823cb66b642eeeca509441c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 12:25:46 +0100

    100-safe_print_integer_err.py fixing some checks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -12,5 +12,3 @@ def safe_print_integer_err(value):
         sys.stderr.write("Exception: {}\n".format(str(err)))
         return False
     return True
-value = 89.9
-print(safe_print_integer_err(value))
