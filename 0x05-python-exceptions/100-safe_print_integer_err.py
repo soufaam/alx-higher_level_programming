bc17f7413a95dcfce75ae8e8b7e60abbdcc29983
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 01:54:10 +0100

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,7 +10,7 @@ def safe_print_integer_err(value):
  'd' for object of type 'str'\n")
         return False
     except TypeError as err:
-        sys.stderr.write("Exception: Unknown format code\
- 'd' for object of type 'str'\n")
+        sys.stderr.write("unsupported format string passed \
+to NoneType.__format__'\n")
         return False
     return True
