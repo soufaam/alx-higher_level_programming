58daaa164766263a00fc6756ea6a421fc97dc221
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 01:49:32 +0100

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,11 +6,25 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as err:
-        sys.stderr.write("Exception: Unknown format code \
-            'd' for object of type 'str'\n")
+        sys.stderr.write("Exception: Unknown format code\
+ 'd' for object of type 'str'\n")
         return False
     except TypeError as err:
         sys.stderr.write("Exception: Unknown format code\
-            'd' for object of type 'str'\n")
+ 'd' for object of type 'str'\n")
         return False
     return True
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
