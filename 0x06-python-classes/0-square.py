46bc6acdf6d68777dce27d6f3cc385f12f40e47e
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 18:49:35 +0100

    0-square.py empty Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,6 +6,8 @@ This module demonstrates documentation as specified by the `Google Python
 Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
 """
+
+
 class Square:
     """This is a class Square summary, it's a empty class that defines a square
     there is no attributes yet
