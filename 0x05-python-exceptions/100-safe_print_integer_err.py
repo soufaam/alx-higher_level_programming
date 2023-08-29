c2f6569bce4ea53df7becbaf735e3a6d2f648cb1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-29 12:20:41 +0100

    100-safe_print_integer_err.py fixing some checks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,11 +6,9 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as err:
-        sys.stderr.write("Exception: Unknown format code\
- 'd' for object of type 'str'\n")
+        sys.stderr.write("Exception: {}\n".write("{}\n".format(str(err))))
         return False
     except TypeError as err:
-        sys.stderr.write("TypeError: unsupported format string passed \
-to NoneType.__format__'\n")
+        sys.stderr.write("Exception: {}\n".format(str(err)))
         return False
     return True
