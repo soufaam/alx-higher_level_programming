b878a5c8dc3f88591c072f5e941f344aada664c0
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-05 00:14:03 +0100

    first fouth task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+"""
+
+
+class Rectangle:
+    """This is a class Rectangle summary, it's a empty class that defines a Rectangle
+    there is no attributes yet
+    """
+    pass
