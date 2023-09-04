d523c164e2bfd64ca49559135d62f41a822bdf15
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-05 00:26:28 +0100

    fix some problems

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 """ This is a decription of this module .
-
 This module demonstrates documentation as specified by the `Google Python
 Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
+0-rectangle module contains Recatangle class implemetation
 """
 
 
