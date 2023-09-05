8500611efe3a941ad512bab4b5880f1c996ecbd1
Soufiane aamari <37306375+soufaam@users.noreply.github.com>
2023-09-05 08:44:03 +0100

    Update 9-rectangle.py
    Replace size by width

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -161,7 +161,7 @@ class Rectangle:
         mentioned here.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         return cls(size, size)
