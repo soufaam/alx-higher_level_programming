5d125a78cca37bc4515e7aeb2b446549071c71fe
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-07 00:51:14 +0100

    Locked Class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,15 +7,18 @@ Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
 """
 class LockedClass:
-    """This is a class Square summary, it's a empty class that defines a square
+    """This is a class Square summary, it's a empty
+    class that defines a square
     there is no attributes yet
     """
     __slots__ = ['first_name']
 
     def __init__(self, fn=None):
         """
-         The __init__ method may be documented in either the class level
-        docstring, or as a docstring on the __init__ method itself.
+         The __init__ method may be documented in
+         either the class level
+        docstring, or as a docstring on the __init__ 
+        method itself.
         Args:
             param1 (str): Description of `param1`.
         """
