8c8ece3fe6c6ca7c962b1b023263a50515434158
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-06 19:20:13 +0100

    fix some checks

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,10 +2,13 @@
 
 """ This is a decription of this module .
 
-This module contains a function called add_integer that checks for th valid given
+This module contains a function called add_integer
+that checks for th valid given
 datatypes and returns a sum of tow integer
-style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
+style Guide`_. Docstrings may extend over
+multiple lines. Sections are created
+with a section header and a colon followed
+by a block of indented text.
 """
 
 
@@ -24,7 +27,8 @@ def matrix_divided(matrix, div):
     size = 0
     i = 0
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of\
+integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, int) and not isinstance(div, float):
@@ -32,15 +36,18 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if isinstance(row, list):
             if i >= 1 and len(row) != size:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix must\
+have the same size")
             size = len(row)
             newrow = []
             for item in row:
                 if not isinstance(item, int) and not isinstance(item, float):
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError("matrix must be a matrix (list of lists)\
+of integers/floats")
                 newrow.append(round(item / div, 2))
         else:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix\
+(list of lists) of integers/floats")
         i += 1
         new_matrix.append(newrow)
     return new_matrix
