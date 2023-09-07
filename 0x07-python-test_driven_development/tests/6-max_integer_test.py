660158d81e9fbf9b95ed86e4a1fec474ad379203
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-07 18:14:22 +0100

    6 Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,56 @@
 #!/bin/usr/python3
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-class TestStringMethods(unittest.TestCase):
+""" This is a decription of this module .
 
-  def test_max_integer(list=[]):
-      self.assertEqual(', 'FOO')
+This module contains a function called text_indentation
+that checks for th valid given
+datatypes and returns a sum of tow integer
+style Guide`_. Docstrings may extend over
+multiple lines. Sections are created
+with a section header and a colon followed
+by a block of indented text.
+"""
 
-  def test_max_integer(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer_normal(self):
+        """Class methods are similar to regular functions.
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Returns:
+            returns 
+        """
+        
+        got = max_integer([1, 2, 4])
+        self.assertEqual(got, 4)
 
-  def test_max_integer(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+    def test_max_integer_void(self):
+        """Class methods are similar to regular functions.
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Returns:
+            returns 
+        """
+        got = max_integer([])
+        self.assertEqual(got, None)
+
+    def test_max_negative(self):
+        """Class methods are similar to regular functions.
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Returns:
+            returns 
+        """
+        got = max_integer([-1, -2, -5, -4])
+        self.assertEqual(got, -1)
+
+        def test_max_negative(self):
+            """Class methods are similar to regular functions.
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Returns:
+            returns
+            """
+        got = max_integer([-1, "3", -5, -4])
+        self.assertEqual(got, -1)
