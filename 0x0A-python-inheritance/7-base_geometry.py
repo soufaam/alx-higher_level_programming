0f90a04d3703aad96e47e54457d6d1babf62b315
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-13 22:56:50 +0100

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+BaseGeometry module contains BaseGeometry class implemetation
+"""
+
+
+class BaseGeometry:
+    """This is a class BaseGeometry summary, it's a empty
+    class that defines a BaseGeometry
+    there is no attributes yet
+    """
+    def area(self):
+        """If the function contains notable behavior, it should be
+    mentioned here.
+    """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """If the function contains notable behavior, it should be
+    mentioned here.
+    """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
