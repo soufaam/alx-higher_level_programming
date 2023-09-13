98697bb949ec8ba36b3d24ec26e376b45f40d030
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-13 18:55:26 +0100

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+BaseGeometry module contains BaseGeometry class implemetation
+"""
+
+
+class BaseGeometry:
+    """This is a class BaseGeometry summary, it's a empty
+    class that defines a BaseGeometry
+    there is no attributes yet
+    """
+    pass
