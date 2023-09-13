ef3f423028d09f2ff2f1e21c6bd443aa1802e613
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-13 18:42:11 +0100

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+
+This module contains a function called is_same_class
+that checks for th valid given
+datatypes and returns a sum of tow integer
+style Guide`_. Docstrings may extend over multiple
+lines. Sections are created
+with a section header and a colon followed by a
+block of indented text.
+"""
+
+
+def inherits_from(obj, a_class):
+    """inherits_from function:
+    args:
+        obj : is a object.
+        a_class: class type
+        If the function contains notable behavior, it should be
+    mentioned here.
+    """
+    return isinstance(obj, a_class)
