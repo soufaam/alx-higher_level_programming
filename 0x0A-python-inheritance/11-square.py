15d4992b0a90311d1edbe8176766c9f2f61abce7
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 00:46:36 +0100

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+BaseGeometry module contains BaseGeometry class implemetation
+"""
+
+
+class BaseGeometry:
+    """This is a class BaseGeometry summary, it's a empty
+    class that defines a BaseGeometry
+    there is no attributes yet
+    """
+    def area(self):
+        """If the function contains notable behavior, it should be
+    mentioned here.
+    """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """If the function contains notable behavior, it should be
+    mentioned here.
+    """
+        if "int" not in str(type(value)):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """This is a class BaseGeometry summary, it's a empty
+    class that defines a BaseGeometry
+    there is no attributes yet
+    """
+    def __init__(self, width, height):
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        """
+        return self.__width * self.__height
+
+    def __str__(self) -> str:
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """This is a class BaseGeometry summary, it's a empty
+    class that defines a BaseGeometry
+    there is no attributes yet
+    Square that inherit from  a Rectangle
+    there is no attributes yet
+    """
+    def __init__(self, size):
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        args:
+            size of rectangle
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        args:
+            size of rectangle
+        """
+        return super().area()
+
+    def __str__(self) -> str:
+        """If the function contains notable behavior, it should be
+        mentioned here.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
