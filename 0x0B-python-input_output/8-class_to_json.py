58a232a9b26adc9fed08f538fe70e159f33fe4f7
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 16:41:33 +0100

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This is a decription of this module .
+this module contains save_to_json_file function that takes myobject
+as parameter
+"""
+from json import dumps
+
+
+def class_to_json(obj):
+    """ This is a decription of this function.
+    This function reads a text file (UTF8) and prints it to stdout:
+    Prototype: def read_file(filename=""):
+    Args:
+    save_to_json_file: jsonify a string"""
+
+    return obj.__dict__
