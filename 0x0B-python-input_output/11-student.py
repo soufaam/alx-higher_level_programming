c85bf3dcc87e4ee19eb4c1bea4f28405109a08e1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 23:58:38 +0100

    implemetation of reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -52,7 +52,7 @@ class Student:
 
     def reload_from_json(self, json):
         """The to_json method may be documented in either the class level"""
-        json["last_name"] = self.last_name
-        json["first_name"] = self.first_name
-        json["age"] = self.age
-        return json
+        print(json)
+        self.last_name = json["last_name"]
+        self.first_name = json["first_name"]
+        self.age = json["age"]
