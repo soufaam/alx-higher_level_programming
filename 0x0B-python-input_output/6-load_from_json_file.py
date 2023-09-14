1115d51adf6679baca6040f0c49b892f4f67c7e2
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 11:24:46 +0100

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This is a decription of this module .
+this module contains save_to_json_file function that takes myobject
+as parameter
+"""
+from json import load
+
+
+def load_from_json_file(filename):
+    """ This is a decription of this function.
+    This function reads a text file (UTF8) and prints it to stdout:
+    Prototype: def read_file(filename=""):
+    Args:
+    save_to_json_file: jsonify a string"""
+
+    with open(filename, "w+") as f:
+        return load(f)
