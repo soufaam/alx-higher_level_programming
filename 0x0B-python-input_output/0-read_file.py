128904bb1f80066544175ca8e8cbcd3d1451ab91
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 10:03:34 +0100

    READ file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module ."""
+
+
+def read_file(filename=""):
+    """ This is a decription of this function.
+    This function reads a text file (UTF8) and prints it to stdout:
+    Prototype: def read_file(filename=""):
+    Args:
+    filname: name of file"""
+    with open (filename) as f:
+        stdout = f.read()
+        print(stdout)
