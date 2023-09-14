f994705ff0dab32524d0f6e2a770a47d6f39ab75
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 23:31:53 +0100

    implemetation of reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+""" This is a decription of this module .
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+"""
+
+
+class Student:
+    """This is a class Square summary, it's a empty class that defines a square
+    there is no attributes yet
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+         The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+        Args:
+            param1 (str): Description of `param1`.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """The to_json method may be documented in either the class level"""
+        data = {}
+        if attrs == []:
+            return data
+        if isinstance(attrs, list):
+            for item in attrs:
+                if isinstance(item, str):
+                    if item == "first_name":
+                        if item not in data.keys():
+                            data[item] = self.first_name
+                    elif item == "last_name":
+                        if item not in data.keys():
+                            data[item] = self.last_name
+                    elif item == "age":
+                        if item not in data.keys():
+                            data[item] = self.age
+                else:
+                    return self.__dict__
+            if data != {}:
+                return data
+            else:
+                return self.__dict__
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """The to_json method may be documented in either the class level"""
+        json["last_name"] = self.last_name
+        json["first_name"] = self.first_name
+        json["age"] = self.age
+        return json
