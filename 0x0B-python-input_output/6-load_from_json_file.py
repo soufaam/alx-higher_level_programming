4ab5e5365d32f5d36abc988bdd4edcd41dc11ff1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-14 16:09:58 +0100

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -13,6 +13,6 @@ def load_from_json_file(filename):
     Args:
     save_to_json_file: jsonify a string"""
 
-    with open(filename, "r+") as f:
+    with open(filename, "r") as f:
         lt = load(f)
     return lt
