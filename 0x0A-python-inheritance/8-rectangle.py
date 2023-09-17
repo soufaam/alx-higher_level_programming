b4a4d8f1058378f5037549e7a41f7ad00a04b877
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-17 18:12:07 +0100

    fix issubcalss return value

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,28 +6,7 @@ Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
 BaseGeometry module contains BaseGeometry class implemetation
 """
-
-
-class BaseGeometry:
-    """This is a class BaseGeometry summary, it's a empty
-    class that defines a BaseGeometry
-    there is no attributes yet
-    """
-    def area(self):
-        """If the function contains notable behavior, it should be
-    mentioned here.
-    """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """If the function contains notable behavior, it should be
-    mentioned here.
-    """
-        if "int" not in str(type(value)):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """This is a class BaseGeometry summary, it's a empty
