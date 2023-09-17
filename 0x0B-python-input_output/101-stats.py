8740af43f1c6a1fba8d1a0c44f342b13fda88776
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-17 15:49:43 +0100

    add if __name == __main__ condition

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -27,10 +27,11 @@ def script_parser():
             status_dict[int(parsedline[-2])] += 1
         list_of_keys = list(status_dict.keys())
         list_of_keys.sort()
-        print(f"File size: {file_size['size']}")
-        if list_of_keys is not None:
-            for code in list_of_keys:
-                print(f"{code}: {status_dict[code]}")
+        if i >= 10:
+            print(f"File size: {file_size['size']}")
+            if list_of_keys is not None:
+                for code in list_of_keys:
+                    print(f"{code}: {status_dict[code]}")
         line = sys.stdin.readline()
 
 
