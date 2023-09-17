be9a8856621403be4f75516de2c8005b64dc1491
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-17 15:32:04 +0100

    add if __name == __main__ condition

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,27 +6,33 @@ as parameter
 
 import sys
 
-status_dict = {}
-file_size = {}
-i = 0
-line = sys.stdin.readline()
-while line != -1:
-    if i == 10:
-        i = 0
-    parsedline = line.split()
-    if "size" not in file_size.keys():
-        file_size["size"] = int(parsedline[-1])
-    else:
-        file_size["size"] += int(parsedline[-1])
-    if int(parsedline[-2]) not in status_dict.keys():
-        status_dict[int(parsedline[-2])] = 1
-    else:
-        status_dict[int(parsedline[-2])] += 1
-    i += 1
-    list_of_keys = list(status_dict.keys())
-    list_of_keys.sort()
-    print(f"File size: {file_size['size']}")
-    if list_of_keys is not None:
-        for code in list_of_keys:
-            print(f"{code}: {status_dict[code]}")
+
+def script_parser():
+    """script parser fhnction"""
+
+    status_dict = {}
+    file_size = {}
+    i = 0
     line = sys.stdin.readline()
+    while line != -1:
+        i += 1
+        parsedline = line.split()
+        if "size" not in file_size.keys():
+            file_size["size"] = int(parsedline[-1])
+        else:
+            file_size["size"] += int(parsedline[-1])
+        if int(parsedline[-2]) not in status_dict.keys():
+            status_dict[int(parsedline[-2])] = 1
+        else:
+            status_dict[int(parsedline[-2])] += 1
+        list_of_keys = list(status_dict.keys())
+        list_of_keys.sort()
+        print(f"File size: {file_size['size']}")
+        if list_of_keys is not None:
+            for code in list_of_keys:
+                print(f"{code}: {status_dict[code]}")
+        line = sys.stdin.readline()
+
+
+if __name__ == "__main__":
+    script_parser()
