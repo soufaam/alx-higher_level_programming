2ba04dd0487383e4ed5bad2ed0d2730c2e3b993c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-09-21 23:24:27 +0100

    class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,16 +25,16 @@ class Rectangle(Base):
 
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if width <= 0:
-            raise ValueError("width must be > 0")
         if x < 0:
             raise ValueError("x must be >= 0")
         if y < 0:
