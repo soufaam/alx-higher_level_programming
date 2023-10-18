acad92cd2261166f83b11ce3aa50884643fb485a
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-19 00:14:20 +0100

    5-unique_id.sql 6-states

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- lists all privileges of the MySQL users user_0d_1 
 -- and user_0d_2 on your server (in localhost).
 CREATE  TABLE IF NOT EXISTS unique_id(
-	id INT,
+	id INT NOT NULL DEFAULT 1,
+	PRIMARY KEY (id),
 	name VARCHAR(256)
 );
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- lists all privileges of the MySQL users user_0d_1 
 -- and user_0d_2 on your server (in localhost).
 CREATE   DATABASE  IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
-CREATE  TABLE IF NOT EXISTS states(
-	id INT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY (id),
 	name VARCHAR(256)
 );
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
