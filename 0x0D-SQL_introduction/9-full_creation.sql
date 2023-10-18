075eeb55353b84775b8a03893d01b29c951b4229
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-18 15:11:25 +0100

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256),
 	score INT
 );
+-- INSERT VALUES in second_table
 INSERT INTO  second_table(id, name, score) VALUES (1, 'John',10),
-(2, 'Alex', 3)
+(2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, "George", 8);
