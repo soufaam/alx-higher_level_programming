34ab59783d6caf00ffc30e155dea60bc13c13483
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-18 11:50:15 +0100

    sql tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,11 @@
 -- script database mysql
 -- Set a placeholder ofthe argument MySQL server.
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO  second_table(id, name, score) 
-VALUES
-(89, 'John',10),
-(2, 'Alex', 3),
-(3, 'BOB', 14),
-(4, "George", 8);
+INSERT INTO  second_table(id, name, score) VALUES (89, 'John',10);
+INSERT INTO  second_table(id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO  second_table(id, name, score) VALUES (3, 'BOB', 14);
+INSERT INTO  second_table(id, name, score) VALUES (4, "George", 8);
