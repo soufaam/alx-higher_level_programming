55370bf8dfc83acbf54098620b795c4f4c9c2cc3
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-18 15:17:40 +0100

    10-top_score.sql order by

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script database mysql
 -- Set a placeholder ofthe argument MySQL server.
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
