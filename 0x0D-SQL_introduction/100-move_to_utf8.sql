554d14ff0a69d96b8839afb522512876645b96d1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-19 14:57:03 +0100

    resolution of task 8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,6 @@
 use hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+MODIFY id INT(11);
 SHOW CREATE TABLE first_table;
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users user_0d_1 
+-- and user_0d_2 on your server (in localhost).
+USE hbtn_0d_usa;
+SELECT id, name, state_id FROM cities 
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id DESC;
