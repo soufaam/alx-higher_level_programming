210733a0b34ab4d72114b44fab5c0ff705a78293
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-19 11:09:18 +0100

    100 advanced task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- script database mysql
 -- Set a placeholder ofthe argument MySQL server.
+use hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SHOW CREATE TABLE first_table;
