ec7cb505a48c08a99d2d5563d2ddb7357b452172
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-19 22:48:16 +0100

    commit for task 9 and 10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 
 -- and user_0d_2 on your server (in localhost).
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows 
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title
-, tv_show_genres.genre_id ASC;
+SELECT cities.id, cities.name, states.name FROM cities 
+LEFT JOIN states
+ON cities.state_id = states.id ORDER BY cities.id ASC;
