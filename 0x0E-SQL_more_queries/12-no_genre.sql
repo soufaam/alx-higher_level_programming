3050fcf6c35b94ec48fb4285cfcbbbe696b2b06c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-19 23:40:30 +0100

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all privileges of the MySQL users user_0d_1 
+-- and user_0d_2 on your server (in localhost).
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows 
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id is NULL ORDER BY tv_shows.title
+, tv_show_genres.genre_id ASC;
