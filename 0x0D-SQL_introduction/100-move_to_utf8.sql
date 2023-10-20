6ecf49bd07ff3aef078f7dcc38155f3b55272d06
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-20 18:21:34 +0100

    advanced task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,5 +5,3 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table
 MODIFY id INT(11);
-SHOW CREATE TABLE first_table;
-/home/soufiane/alx-higher_level_programming/0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
