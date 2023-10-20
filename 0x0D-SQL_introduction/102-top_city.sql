6536e49b6bd7aae8160f1aae82d3995ebe88b4dc
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-20 21:53:40 +0100

    Advanced task 102 average temperatures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- script database mysql
+-- Set a placeholder ofthe argument MySQL server.
+USE hbtn_0c_0;
+SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures
+GROUP BY temperatures.city ORDER BY avg_temp DESC LIMIT 3;
