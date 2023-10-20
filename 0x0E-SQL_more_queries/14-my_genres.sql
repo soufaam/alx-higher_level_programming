e557032038d885ef3674930e14a602ed95f6b786
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-21 00:34:57 +0100

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- lists all privileges of the MySQL users user_0d_1 
+-- and user_0d_2 on your server (in localhost).
+
+SELECT tv_genres.name AS name FROM tv_shows 
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id 
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+WHERE tv_shows.title = 'Dexter'
+;
