35a151a3ee5303f3724e7a34a21c20b29187cb2a
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-20 22:05:48 +0100

    Advanced task 102 average temperatures

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,4 +2,5 @@
 -- Set a placeholder ofthe argument MySQL server.
 USE hbtn_0c_0;
 SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures
+WHERE temperatures.month = 7 OR temperatures.month = 8
 GROUP BY temperatures.city ORDER BY avg_temp DESC LIMIT 3;
