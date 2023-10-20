302a13c279fb9c4ab9fc27f4d0caa9a6f6a9f5b9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-20 21:47:41 +0100

    Advanced task 101 average temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- script database mysql
 -- Set a placeholder ofthe argument MySQL server.
-use hbtn_0c_0;
-SELECT temperatures.city, AVG(temperatures.value) as avg_temp from temperatures
+USE hbtn_0c_0;
+SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures
 GROUP BY temperatures.city ORDER BY avg_temp DESC;
