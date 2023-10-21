1ca1258aeed6623935bbf00eb47647c4acafa62f
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-21 23:21:02 +0100

    100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- lists all privileges of the MySQL users user_0d_1 
+-- and user_0d_2 on your server (in localhost).
+
+SELECT  tv_genres.name  AS name FROM tv_genres 
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id 
+INNER JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id 
+AND tv_shows.title = 'Dexter' WHERE  tv_shows.id  is NULL
+;
