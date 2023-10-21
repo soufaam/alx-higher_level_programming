bb60d0f8e9c2272c3ae78f2dd57d1145500e6cff
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-21 23:23:43 +0100

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,5 +5,4 @@ SELECT  tv_genres.name  AS name FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id 
 INNER JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id 
-AND tv_shows.title = 'Dexter' WHERE  tv_shows.id  is NULL
-;
+AND tv_shows.title = 'Dexter' ORDER BY tv_genres.name ;
