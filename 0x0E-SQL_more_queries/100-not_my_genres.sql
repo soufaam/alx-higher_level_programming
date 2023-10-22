9faac1b9440fc1efbf0e60e621a762278e1162aa
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-10-22 13:03:33 +0100

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,9 @@
 -- lists all privileges of the MySQL users user_0d_1 
 -- and user_0d_2 on your server (in localhost).
-
-SELECT  tv_genres.name  AS name FROM tv_genres 
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id 
-INNER JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id 
-AND tv_shows.title = 'Dexter' ORDER BY tv_genres.name ;
+USE hbtn_test_db_shows;
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title IS NULL OR tv_shows.title != 'Dexter'
+ORDER BY tv_genres.name ASC;
