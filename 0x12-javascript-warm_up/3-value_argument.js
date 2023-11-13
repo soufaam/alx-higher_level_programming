22eec7c6415ff5aae2faeb453ea2a1ffe0851932
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-13 16:56:49 +0100

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,5 @@ const { argv } = require('node:process');
 if (argv.length === 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+  console.log(argv[2]);
 }
