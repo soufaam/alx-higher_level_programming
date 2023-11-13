2cb7cbf80fdee8f0be1bea0e24b204fa133e12a8
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-13 21:19:28 +0100

    argument and Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length === 2) {
+if (process.argv[2] == undefined){
   console.log('No argument');
-} else if (argv.length === 3) {
-  console.log(argv[2]);
+} else {
+  console.log(process.argv[2]);
 }
