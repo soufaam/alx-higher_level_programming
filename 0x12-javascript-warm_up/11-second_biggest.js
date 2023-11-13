f4d2caf8504447611b2e4223ca500579fbc17d3c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-13 23:07:44 +0100

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (argv.length === 2) {
+  console.log(0);
+} else if (argv.length === 3) {
+  console.log(0);
+} else {
+  let big = parseInt(argv[2]);
+
+  for (let j = 2; j < argv.length; j++) {
+    const num = parseInt(argv[j]);
+    if (big < num) {
+      big = num;
+    }
+  }
+  console.log(big);
+}
