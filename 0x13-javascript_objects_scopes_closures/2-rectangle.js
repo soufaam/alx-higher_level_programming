7b994efcd249b36138f2264bde6627fb27707f59
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-14 22:01:28 +0100

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (Number.isInteger(w) && w > 0 && !Number.isInteger(h) && h > 0) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
       this.width = w;
       this.height = h;
     }
