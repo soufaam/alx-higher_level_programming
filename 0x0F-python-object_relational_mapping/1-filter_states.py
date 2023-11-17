a0c767a1213ed1938e04b9933f18b6acec295282
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-17 23:08:42 +0100

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A python3  script that lists all states from the
+database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    """connect to the MYSQL SERVER"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    command_sql = 'SELECT * FROM states WHERE name LIKE "%A"  ORDER BY states.id'
+    result = cur.execute(command_sql)
+    for row in cur.fetchall():
+        print(row)
