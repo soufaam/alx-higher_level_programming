e0fa216de165d38251ac2c978bcf4b33626a5a57
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-17 22:58:17 +0100

    README file and task1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 import sys
 
 """connect to the MYSQL SERVER"""
-db = MySQLdb.connect(host='127.0.0.1', port=13306,
+db = MySQLdb.connect(host='127.0.0.1', port=3306,
                      user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 command_sql = 'SELECT * FROM states ORDER BY states.id'
