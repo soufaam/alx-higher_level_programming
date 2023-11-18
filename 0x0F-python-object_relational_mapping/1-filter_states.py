badd1c3dab7118bbac4d78f97a56cb7834bf17a1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-18 22:56:04 +0100

    task2 and task2 fix bugs

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     command_sql = 'SELECT * FROM states WHERE states.name LIKE "N%" \
-ORDER BY states.id ASC'
+ORDER BY states.id ASC;'
     result = cur.execute(command_sql)
     for row in cur.fetchall():
         print(row)
+    cur.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    command_sql = "SELECT * FROM states WHERE states.name = '{}' \
-ORDER BY states.id ASC".format(sys.argv[4])
+    command_sql = "SELECT * FROM states WHERE name = '{}' \
+ORDER BY states.id ASC;".format(sys.argv[4])
     result = cur.execute(command_sql)
     for row in cur.fetchall():
         print(row)
+    cur.close()
