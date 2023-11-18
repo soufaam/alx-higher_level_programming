5de13e9c84d64655753a62850f122830cd4f00e7
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-18 23:21:24 +0100

    task 3 safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A python3  script that lists all states from the
+database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    """connect to the MYSQL SERVER"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    param = (sys.argv[4],)
+    command_sql = "SELECT * FROM states WHERE name = '%s \
+ORDER BY states.id ASC;"
+    result = cur.execute(command_sql, param)
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
