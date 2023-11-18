edd3fdd94b6155915919b92c9a6e367c0b2ad3cb
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-18 23:47:58 +0100

    task 3 fix bugs

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     import sys
 
     """connect to the MYSQL SERVER"""
-    db = MySQLdb.connect(host='127.0.0.1', port=13306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    param = (sys.argv[4],)
-    command_sql = "SELECT * FROM states WHERE states.name = %s \
+    param = sys.argv[4]
+    command_sql = "SELECT * FROM states WHERE states.name = %(param)s \
 ORDER BY states.id ASC;"
-    result = cur.execute(command_sql, param)
+    result = cur.execute(command_sql, {'param':param})
     for row in cur.fetchall():
         print(row)
     cur.close()
