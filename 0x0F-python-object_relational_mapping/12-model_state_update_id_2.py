d907cce8113e6a2b0b9dd5f865c48c4f0a88d18c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-20 00:26:52 +0100

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""script that adds the State object
+“Louisiana” to the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
+    """The entry point where the script will executed"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     factory = sessionmaker(bind=engine)
     session = factory()
-    for instance in session.query(State):
-        print("{}: {}".format(instance.id, instance.name))
+    result = session.query(State).filter(State.id == 2)
+    record = result.one()
+    record.name = "New Mexico"
+    session.commit()
