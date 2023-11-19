ff8bea3da7a6552a98cfdeabfba84e0f7ec89e68
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-19 23:01:17 +0100

    First state query mysql

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""a script that prints the first State object
+from the database
 """
 import sys
 from model_state import Base, State
@@ -7,12 +8,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    """The entry point where the script will executed"""
+    engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1:13306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     factory = sessionmaker(bind=engine)
     session = factory()
     idx = 1
-    for instance in session.query(State).filter_by(id=1):
+    flag = False
+    for instance in session.query(State).filter_by(id=122):
+        flag = True
         print("{}: {}".format(idx, instance.name))
         idx += 1
+    if not flag:
+        print("Nothing")
