0d33ee05e2ad6cc612638f9f6667ff5fd248afa9
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-19 12:14:21 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     """connect to the MYSQL SERVER"""
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     command_sql = "SELECT * FROM states WHERE states.name = '{}' \
 ORDER BY states.id ASC;".format(sys.argv[4])
