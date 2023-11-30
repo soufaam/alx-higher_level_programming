cee666d02a7581e080c690762005d8034e13a936
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-30 23:11:37 +0100

    GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Display only body of a 200 status code response
-status=$(curl -s -w "%{http_code}"  -o /dev/null "$1")
-if [ "$status" -eq 200 ]; then
-curl -s "$1"
-fi
+curl -s -X GET "$1"
+
