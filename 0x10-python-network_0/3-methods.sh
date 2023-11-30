cc659ee0fc9ebf53ad9d095518e68be07516470d
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-30 23:49:38 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Display only body of a 200 status code response
-curl -I $1 2>/dev/null | awk '/Allow:/ {print $2}'
+curl -I $1 2>/dev/null | awk '/Allow:/ {print substr($0, index($0,$2))}'
