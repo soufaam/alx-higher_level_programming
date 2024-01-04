c79424ed78ca5189ac50919d241bb0c5825ce15b
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-01-04 21:43:22 +0100

    fix error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,7 @@ request(process.argv[2], (error, response) => {
   if (error) {
     console.log(error.code);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
+ 
